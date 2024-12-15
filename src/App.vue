5462226c4bf64ba7a1796a2e8bc949b1dd7013ae
Alija Sabic <sabic.alija@gmail.com>
2024-12-16 00:38:14 +0100

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
-  <div id="app" :class="{ dark: isDark }">
+  <div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/dashboard">Dashboard</router-link></li>
+        <li>
+          <button @click="toggleDarkMode">{{ isDark ? "Light" : "Dark" }} Mode</button>
+        </li>
+      </ul>
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script setup>
-import { useDark, useToggle } from "@vueuse/core";
+import { useDark } from "@vueuse/core";
 
 const isDark = useDark();
-const toggleDarkMode = useToggle(isDark);
+
+const toggleDarkMode = () => {
+  isDark.value = !isDark.value;
+  if (isDark.value) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+};
 </script>
+
+<style>
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline;
+  margin-right: 10px;
+}
+</style>
