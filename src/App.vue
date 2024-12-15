0a97a0caa071ca0b853134e1348fe6a730aa8131
Alija Sabic <sabic.alija@gmail.com>
2024-12-15 23:12:04 +0100

    Add vue-use and add style for dark mode users

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ dark: isDark }">
     <router-view />
   </div>
 </template>
+
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDarkMode = useToggle(isDark);
+</script>
