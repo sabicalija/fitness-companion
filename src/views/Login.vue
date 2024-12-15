ce10ce85ce1ce3676078cc0d27b983ab1d46dca6
Alija Sabic <sabic.alija@gmail.com>
2024-12-15 01:10:11 +0100

    Implement basic login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="login">
+    <h1>Welcome to Fitness Companion</h1>
+    <p>Track your fitness data seamlessly.</p>
+    <button @click="handleLogin">Login with Google</button>
   </div>
 </template>
+
+<script setup>
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+
+const handleLogin = () => {
+  authStore.login();
+};
+</script>
